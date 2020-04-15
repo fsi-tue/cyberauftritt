903d465c1615db11c91861aaa9d70491606830a4
Lukas Oertel <git@luoe.dev>
2020-04-15 19:34:09 +0200

    Fix JS not working
    window.location.port will be empty if port is 80 or 443.
    Do not think about it.

--- js/convertPageLinks.js
+++ js/convertPageLinks.js
@@ -1,9 +1,16 @@
 // get all HTML anchors
 var allAnchors = document.getElementsByTagName("a");
 
+// if the port is 80 or 443, window.location.port will be empty
+if (window.location.port == '' || window.location.port == null){
+	var portOfPage = "/";
+} else {
+	var portOfPage = ":" + window.location.port + "/";
+}
+
 // get the current protocol, host and port
 // (this could be set to https://www.fsi.uni-tuebingen.de/ but will break when testing the website locally)
-var protoHostPort = window.location.protocol + "//" + window.location.hostname + ":" + window.location.port + "/";
+var protoHostPort = window.location.protocol + "//" + window.location.hostname + portOfPage;
 
 // replace all internal page links with absolute links because TOC uses relative links and
 // due to the fact that <base href="/"> is set this would link to the startpage ("/")
